0a7cef7e7bb76c05e8db6c6466d872c10ab43d4d
Ernesto <ernestopraxis@gmail.com>
2026-05-24 03:53:06 -0300

    Agregar Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Image from "next/image";
 
@@ -35,6 +36,7 @@ export default function RootLayout({
         <div className="content-wrapper flex flex-col flex-1">
           {children}
         </div>
+        <Analytics />
       </body>
     </html>
   );
